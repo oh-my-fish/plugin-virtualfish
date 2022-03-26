fe030a801608cd0c3398407a119910e5d7ff8656
Dennis Webb <dhwebb@gmail.com>
2022-03-26 16:30:37 -0500

    updates for compatibility with fish v3.4.0 (#6)
    * updates for compatibility with fish v3.4.0
    
    * Update conf.d/virtualfish.fish
    
    Co-authored-by: Pablo Aguiar <scorphus@gmail.com>

--- conf.d/virtualfish.fish
+++ conf.d/virtualfish.fish
@@ -1,12 +1,11 @@
-if set -q $VIRTUALFISH_PYTHON
-    set VIRTUALFISH_PYTHON python
-end
+set -q VIRTUALFISH_PYTHON
+or set VIRTUALFISH_PYTHON python
 
 eval (eval $VIRTUALFISH_PYTHON -m virtualfish $VIRTUALFISH_PLUGINS 2>/dev/null)
 
 if test $status -gt 0
     echo "Please install virtualfish by running `pip install virtualfish`"
-    echo "and make sure you've set $VIRTUALFISH_PYTHON to the correct"
+    echo "and make sure you've set \$VIRTUALFISH_PYTHON to the correct"
     echo "interpreter"
     echo "See http://virtualfish.readthedocs.io/en/latest/install.html"
 end
